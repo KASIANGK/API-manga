27e458345898ec039bb217ff37f4da2eb8fcd3bc
Kasia_Ngk <ngk.kasia@gmail.com~>
2024-03-11 20:00:36 +0100

    DetailAnime content, 2e API - opening and ending

--- src/Components/DetailAnime/DetailAnime.jsx
+++ src/Components/DetailAnime/DetailAnime.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
@@ -6,16 +6,21 @@ function DetailAnime() {
 
   // DECLARATION useState()
   const [animeDetails, setAnimeDetails] = useState(null)
-  // DECLARATION useParams()
+  const [opening, setOpening] = useState(null)
   const { id } = useParams()
 
-  // IMPORT API
+  // IMPORT DEUX API
   useEffect(() => {
     const importAnimeDetails = async () => {
       try {
         const response = await fetch(`https://api.jikan.moe/v4/anime/${id}`)
-        const data = await response.json();
+        const data = await response.json()
         setAnimeDetails(data.data)
+
+        const openingResponse = await fetch(`https://api.jikan.moe/v4/anime/${id}/themes`)
+        const openingJson = await openingResponse.json()
+        setOpening(openingJson.data)
+
       } catch (error) {
         console.error(error)
       }
@@ -26,13 +31,51 @@ function DetailAnime() {
   // BODY COMPONENT DetailAnime
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
       <p>Detail Anime</p>
-      {animeDetails ? ( 
+
+      {animeDetails ? (
         <div className="detail-anime">
-          <p>{animeDetails.title}</p>
-          <img src={animeDetails.images.jpg.image_url} alt={animeDetails.title} />
+          <p>{animeDetails.titles[0].title}</p>
+          <p>{animeDetails.title_japanese}</p>
+          <p>SCORE : {animeDetails.score}</p>
+          <p>STUDIO : {animeDetails.studios[0].name}</p>
+          <p>GENRE(S) : 
+            {animeDetails.genres.map((element, i) => (
+              <span key={i}>
+                <p>{element.name}</p>
+              </span>
+            ))}
+          </p>
+          <img src={animeDetails.images.jpg.image_url} />
           <p>{animeDetails.synopsis}</p>
+
+        
+          {opening && opening.openings.length > 0 && (
+            <div>
+              <p>OPENING :</p>
+              <ul>
+                {opening.openings.map((element, i) => (
+                  <li key={i}>
+                  {element}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {opening && opening.endings.length > 0 && (
+            <div>
+              <p>ENDING :</p>
+              <ul>
+                {opening.endings.map((element, i) => (
+                  <li key={i}>
+                  {element}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       ) : (
         <p>OOOPS</p>
